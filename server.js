7ec0bfb438669e7adbddc69452dd08f831994a67
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-07-07 14:43:18 -0600

    outputting env vars

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('Containers rule!!!!!');
+    console.log(`My favorite food is ${process.env.FAVORITE_FOOD}`);
     await sleep(5000);
   }
 }
